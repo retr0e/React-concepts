b05a0eecaee75d6eb2b5a15b87d663a1b03de2c6
Dawid Śleziak <dawid.sleziak@gmail.com>
2024-05-12 21:50:15 +0200

    Proper way to lift of a state
    Makeing one state with greater dose of information instead of duplication of lifted state

--- 02-React essentials (deep dive)/src/App.jsx
+++ 02-React essentials (deep dive)/src/App.jsx
@@ -1,14 +1,33 @@
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
+
 import { useState } from "react";
 
 function App() {
   // Lifting the state up (over here)
   // to manage the information that is needed in multiple components
   const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currentlyActivePlayer) => (currentlyActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      // It is not a good idea to use another state in the state update
+      // For example if we use "activePlayer" state instead of solution below
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -23,7 +42,7 @@ function App() {
         {/* Lifting the state up (over here pt.3) */}
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
--- 02-React essentials (deep dive)/src/components/GameBoard.jsx
+++ 02-React essentials (deep dive)/src/components/GameBoard.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+export default function GameBoard({ onSelectSquare }) {
+  /*
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -21,6 +20,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
 
     onSelectSquare();
   }
+  */
 
   return (
     <ol id='game-board'>
@@ -29,7 +29,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={onSelectSquare}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
